af8724432c6e48eaa04577b0381fc1e1e7b9eb97
yamashita <yamashita.antapp@gmail.com>
2022-08-30 11:33:19 +0900

    ✨feature_bootstrapのグリッドシステムのみ導入_ページ幅の設定変更

--- htdocs/wordpress/wp-content/themes/antapp/functions.php
+++ htdocs/wordpress/wp-content/themes/antapp/functions.php
@@ -10,28 +10,57 @@
  */
 
 
-if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+// bootstrap.min.css の読み込み
+function add_my_styles_and_scripts() {
+    wp_enqueue_style(
+        'my-template-bs-style',
+        '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' ,
+        array(),
+        '5.2.0'
+    );
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_support() {
+    //スタイルシート style.css の読み込み
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'add_my_styles_and_scripts' );
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+//$custom_header = array(
+//    'default-image' => get_template_directory_uri().'/images/header-img.jpg',
+//    'width' => 1000,
+//    'height' => 400,
+//    'flex-height' => true,
+//    'flex-width' => false,
+//    'uploads' => true,
+//);
+//add_theme_support( 'custom-header', $custom_header);
+//if(function_exists('register_nav_menu')) {
+//    register_nav_menu('global-menu',__( 'Global menu', 'tcd-w'));
+//}
 
-	}
-
-endif;
-
-add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+//if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+//
+//	/**
+//	 * Sets up theme defaults and registers support for various WordPress features.
+//	 *
+//	 * @since Twenty Twenty-Two 1.0
+//	 *
+//	 * @return void
+//	 */
+//	function twentytwentytwo_support() {
+//
+//		// Add support for block styles.
+//		add_theme_support( 'wp-block-styles' );
+//
+//		// Enqueue editor styles.
+//		add_editor_style( 'style.css' );
+//
+//	}
+//
+//endif;
+//
+//add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 
 if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
