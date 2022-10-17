35b9d6e3e9caaaf179696475b420a62a457493aa
yamashita <yamashita.antapp@gmail.com>
2022-10-17 10:21:06 +0900

    ✨feature_ホーム画面動画配置完成_背景固定

--- htdocs/wordpress/wp-content/themes/antapp/js/index.js
+++ htdocs/wordpress/wp-content/themes/antapp/js/index.js
@@ -50,13 +50,97 @@ gsap.from('.service-web-flow-text',
 // const windowWidth = window.innerWidth;
 // let contentWidth = document.getElementById('content');
 // console.log(contentWidth);
+
+// gsap.set(".home-top", {opacity:1, y:10}); //初期状態をセット
+
+// ScrollTrigger.batch(".home-top", {
+//     onEnter: batch => gsap.to(batch, {opacity: 0, y:0}),
+//     start: "bottom 70%",
+//     once: true,
+//     toggleActions: 'reverse'
+// });
+
+
+
+const back_anime = gsap.timeline({
+    toggleActions: 'reverse', //デフォルトの指定
+    scrollTrigger: {
+        trigger: ".home-top-spacer", //アニメーションが始まるトリガーとなる要素
+        start: "top top",
+        end: "bottom top",
+        scrub: 1.5,
+    }
+});
+back_anime.fromTo(".home-top",
+    {
+        autoAlpha: 1,
+        y:0,
+    }, {
+        autoAlpha: 0,
+        y:20,
+    });
+// back_anime.fromTo(".circle",
+//     {
+//         // autoAlpha: 1,
+//         x: 0,
+//         y: 0,
+//     }, {
+//         // autoAlpha: 0,
+//         x:window.innerWidth / 7,
+//         y:100,
+//     });
+// back_anime.fromTo(".back1",
+//     {
+//         // autoAlpha: 1,
+//         x:0,
+//         y:0,
+//     }, {
+//         // autoAlpha: 0,
+//         x: - (window.innerWidth),
+//         y: -window.innerHeight / 2
+//     });
+// back_anime.fromTo(".back2",
+//     {
+//         autoAlpha: 0,
+//         x: - (window.innerWidth),
+//     }, {
+//         autoAlpha: 1,
+//         x:0,
+//     });
+
+
+// const back_anime2 = gsap.timeline({
+//     toggleActions: 'reverse', //デフォルトの指定
+//     scrollTrigger: {
+//         trigger: ".service-web", //アニメーションが始まるトリガーとなる要素
+//         start: "top top",
+//         end: "bottom top",
+//         scrub: 1.5,
+//     }
+// });
+// back_anime2.fromTo(".circle",
+//     {
+//         x:window.innerWidth / 7,
+//         y:100,
+//     }, {
+//         x:- window.innerWidth / 5,
+//         y: window.innerWidth / 10,
+//     });
+// back_anime2.fromTo(".back1",
+//     {
+//         x: - (window.innerWidth),
+//         y: -window.innerHeight / 2
+//     }, {
+//         x: (window.innerWidth / 2),
+//         y: -window.innerHeight / 2
+//     });
+
 gsap.from('.service-web',
     {
-        // y: 0,
-        // x:-windowWidth/8,
+        y: 20,
         autoAlpha: 0,
         delay: 0.0,
-        duration: 1.0,
+        duration: 2.0,
         ease: 'power4.0in',
         scrollTrigger: {
             trigger: '.service-web',
@@ -65,11 +149,10 @@ gsap.from('.service-web',
     });
 gsap.from('.service-marketing',
     {
-        // y: 0,
-        // x:-windowWidth/8,
+        y: 20,
         autoAlpha: 0,
         delay: 0.0,
-        duration: 1.0,
+        duration: 2.0,
         ease: 'power4.0in',
         scrollTrigger: {
             trigger: '.service-marketing',
@@ -78,9 +161,11 @@ gsap.from('.service-marketing',
     });
 gsap.from('.strength',
     {
+        y: 10,
+        autoAlpha: 0,
         autoAlpha: 0,
         delay: 0.0,
-        duration: 1.0,
+        duration: 2.0,
         ease: 'power4.0in',
         scrollTrigger: {
             trigger: '.service',
